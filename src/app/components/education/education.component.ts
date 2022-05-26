3e116f36d2da598f9b8a92a22b6d15124e603159
Andres Turano <turamdq@gmail.com>
2022-05-26 18:35:13 -0300

    Funcion de botones añadidas

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PortfolioService } from '../../services/portfolio.service';
+// import { Portfolio } from '../../interfaces/portfolio'; APLICAR INTERFACE !!!!!!
 
 @Component({
   selector: 'app-education',
@@ -13,20 +14,62 @@ export class EducationComponent implements OnInit {
   
   educationList: any;
 
+  nuevoId: number = 0;
+
+  id:string = "";
+  name:string="";
+  title: string = "";
+  description:string = "";
+  image:string = ""; 
+  
+
   constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
-    this.leerDatos()
-    // this.datosPortfolio.obtenerDatos().subscribe(data => {
-    //   this.educationList = data.education;
-    // })
+    this.leerDatos();    
   }
 
   //Funcion para obtener datos mediante el servicio
+
   leerDatos(){
     this.datosPortfolio.obtenerDatos(this.url).subscribe((response)=>{
       this.educationList = response;      
     });
   }
 
+    //Funcion para obtener datos mediante el servicio
+
+    agregarItem() {
+      // console.log(this.name);
+      this.nuevoId = new Date().getTime();      //Genera un numero basado en la fecha    
+      const body = {id: this.nuevoId, name: this.name, title: this.title, description: this.description, image: this.image}
+      this.datosPortfolio.agregarNuevo(this.url, body).subscribe();
+    }
+  
+    borrarItem(){         
+      this.datosPortfolio.borrarDatos(this.url, this.id).subscribe();
+      this.leerDatos();    
+    }
+  
+    //Obtiene los datos a modificar o el ID del elemento a eliminar
+  
+    itemAModificar(education:any){
+      this.id = `${education.id}`;
+      this.name = `${education.name}`;
+      this.title = `${education.title}`;
+      this.description = `${education.period}`;
+      this.image = `${education.image}`;
+    }
+  
+    guardarCambios(){
+      const body = {id: this.id, name: this.name, title: this.title, description: this.description, image: this.image}      
+      this.datosPortfolio.modificarDatos(this.url, body).subscribe();
+      this.leerDatos();
+    }
+  
+    //vuelve a Cargar los datos guardados en la BDD
+    
+    descartarCambios() {
+      return this.leerDatos();    
+    }
 }
