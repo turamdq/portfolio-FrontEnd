e74cca162ea03655d05cb884834eeec9eeb90123
Andres Turano <turamdq@gmail.com>
2022-08-14 23:04:37 -0300

    Solicuinado error conflicto git

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  
-=======
   {path:'', component: HomeComponent},
   {path:'login', component: LoginComponent}
->>>>>>> dev
 ];
 
 @NgModule({
