3525028df750afce472acfa62d6303cf4b94dc5c
Andres Turano <turamdq@gmail.com>
2022-08-22 21:11:29 -0300

    prueba

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  {path:'', component: HomeComponent},
-  {path:'login', component: LoginComponent}
+  {path:'', component: HomeComponent}
+  //{path:'login', component: LoginComponent}
 ];
 
 @NgModule({
